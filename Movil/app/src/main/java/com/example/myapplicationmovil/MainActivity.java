530b29833c9a365bf846919979b61132ff42a9df
Ricardo Marquez <r.marquez18@info.uas.edu.mx>
2022-11-04 07:54:44 -0700

    Nuevos cambios en el registro y login en la parte del Bak-End

--- Movil/app/src/main/java/com/example/myapplicationmovil/MainActivity.java
+++ Movil/app/src/main/java/com/example/myapplicationmovil/MainActivity.java
@@ -11,9 +11,11 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView btn_Registrar;
+import com.google.android.material.textfield.TextInputLayout;
 
+public class MainActivity extends AppCompatActivity {
+    private TextView btn_iniciar,btn_registrar;
+    private TextInputLayout til_correo, til_password;
     /*
         Host: sql.freedb.tech
         port: 3306
@@ -28,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Inicializadores();
         Iniciar();
+        Registrar();
         Barra();
         Ventana();
     }//FIN DEL ONCREATE
@@ -45,15 +48,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void Inicializadores(){
-        btn_Registrar = (TextView) findViewById(R.id.btn_Registrar_Login);
+        btn_iniciar = (Button) findViewById(R.id.btn_Iniciar);
+        btn_registrar = (TextView) findViewById(R.id.btn_Registrar_Login);
+        til_correo = (TextInputLayout) findViewById(R.id.til_Correo);
+        til_password = (TextInputLayout) findViewById(R.id.til_Password);
     }
 
     private void Iniciar(){
-        btn_Registrar.setOnClickListener(new View.OnClickListener() {
+        btn_iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s_correo_login = "";
+                String s_password_login = "";
+                s_correo_login = til_correo.getEditText().getText().toString();
+                s_password_login = til_password.getEditText().getText().toString();
+                if(s_correo_login.isEmpty() || !s_correo_login.contains("@")){
+                    ShowError(til_correo, "Correo Incorrecto");
+                }if(s_password_login.isEmpty() || s_password_login.length()<6){
+                    ShowError(til_password,"Contraseña Incorrecta");
+                }else {
+                    //Aqui va la funcion para iniciar sesion
+                }
+            }
+        });
+    }
+
+    private void Registrar(){
+        btn_registrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this,RegistrarUsuario.class));
             }
         });
     }
+
+    //FUNCION DE ERROR
+    private void ShowError(TextInputLayout inputEditText, String s){
+        inputEditText.setError(s);
+        inputEditText.requestFocus();
+    }//FIN DE LA FUNCION ERROR
 }
--- Movil/app/src/main/java/com/example/myapplicationmovil/RegistrarUsuario.java
+++ Movil/app/src/main/java/com/example/myapplicationmovil/RegistrarUsuario.java
@@ -6,13 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class RegistrarUsuario extends AppCompatActivity {
+    private TextInputLayout til_correo_r,til_usuario_r,til_password_r;
+    private Button btn_registrar_r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_usuario);
+        Inicializadores();
+        Registrar_Usuario();
         Barra();
         Ventana();
     }//FIN DEL ONCREATE
@@ -31,9 +38,41 @@ public class RegistrarUsuario extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
+    private void Inicializadores(){
+        til_correo_r = (TextInputLayout) findViewById(R.id.til_Correo_Registro);
+        til_usuario_r = (TextInputLayout) findViewById(R.id.til_Usuario);
+        til_password_r = (TextInputLayout) findViewById(R.id.til_Password_Registro);
+        btn_registrar_r = (Button) findViewById(R.id.btn_Registrar);
+    }
+
+    private void Registrar_Usuario(){
+        String s_correo_r = "";
+        String s_usuario_r = "";
+        String s_password_r = "";
+        s_correo_r = til_correo_r.getEditText().getText().toString();
+        s_usuario_r = til_usuario_r.getEditText().getText().toString();
+        s_password_r = til_password_r.getEditText().getText().toString();
+
+        if(s_correo_r.isEmpty() || !s_correo_r.contains("@")){
+            ShowError(til_correo_r, "Correo Incorrecto");
+        }if(s_usuario_r.isEmpty() || s_usuario_r.length() < 5){
+            ShowError(til_usuario_r, "Nombre de Usuario corto");
+        }if(s_password_r.isEmpty() || s_password_r.length()<6){
+            ShowError(til_password_r, "Contraseña Muy Debil");
+        }else {
+            //Aquiva el codigo del registro
+        }
+    }
+
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return super.onSupportNavigateUp();
     }
+
+    //FUNCION DE ERROR
+    private void ShowError(TextInputLayout inputEditText, String s){
+        inputEditText.setError(s);
+        inputEditText.requestFocus();
+    }//FIN DE LA FUNCION ERROR
 }
